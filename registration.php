c6f0f56f67134270d967f65acceea4e3e4b8f359
danny329 <danielpaul329@outlook.com>
2019-01-04 01:44:45 +0530

    registration page work

--- registration.php
+++ registration.php
@@ -5,5 +5,8 @@
                     <img src="images/icons/undraw_working_remotely_jh40.svg" alt="registration" width="30%">
                     <h2 class="text-center text-white">COMING SOON!</h2>
                 </div>
-        </div>
+</div>
+<div class="mainreg">
+    <br><br><h1>hello world</h1>
+</div>
         
